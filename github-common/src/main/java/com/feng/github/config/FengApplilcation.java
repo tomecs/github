0750eeae36e772d91198954962bd34b6da3a2112
fengmenglei <fengmenglei@vortexinfo.cn>
2018-02-10 15:06:10 +0800

    github-task 2 rebase test
    Signed-off-by: fengmenglei <fengmenglei@vortexinfo.cn>

--- github-common/src/main/java/com/feng/github/config/FengApplilcation.java
+++ github-common/src/main/java/com/feng/github/config/FengApplilcation.java
@@ -1,6 +1,5 @@
 package com.feng.github.config;
 
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
