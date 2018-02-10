7dbf112cc60f8941e658e2ca42aafbf9ae7e7d4e
fengmenglei <fengmenglei@vortexinfo.cn>
2018-02-10 14:59:00 +0800

    github-task 1 rebase test
    Signed-off-by: fengmenglei <fengmenglei@vortexinfo.cn>

---
+++ github-task/src/main/java/feng/GithubTaskApplication.java
@@ -0,0 +1,12 @@
+package feng;
+
+import com.feng.github.config.FengApplilcation;
+
+/**
+ * @description:
+ * @author: fengmenglei
+ * @date: 2018/2/10
+ */
+@FengApplilcation
+public class GithubTaskApplication {
+}
