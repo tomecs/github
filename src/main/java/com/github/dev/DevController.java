e86b112617a71511511569242b921983e23b64b3
fengmenglei <fengmenglei@vortexinfo.cn>
2018-02-10 14:32:37 +0800

    全部提交
    Signed-off-by: fengmenglei <fengmenglei@vortexinfo.cn>

--- src/main/java/com/github/dev/DevController.java
+++
@@ -1,9 +0,0 @@
-package com.github.dev;
-
-/**
- * @description: dev分支
- * @author: fengmenglei
- * @date: 2018/2/9
- */
-public class DevController {
-}
--- src/main/java/com/github/dev/boots/BootsController.java
+++
@@ -1,9 +0,0 @@
-package com.github.dev.boots;
-
-/**
- * @description:
- * @author: fengmenglei
- * @date: 2018/2/9
- */
-public class BootsController {
-}
